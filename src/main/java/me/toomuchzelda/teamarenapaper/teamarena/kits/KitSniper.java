20a9f7442f6109cacb6bf6f35d33b0bf865e4f94
onett <99157739+onett425@users.noreply.github.com>
2022-04-22 22:08:19 -0400

    Sniper Progress

--- src/main/java/me/toomuchzelda/teamarenapaper/teamarena/kits/KitSniper.java
+++ src/main/java/me/toomuchzelda/teamarenapaper/teamarena/kits/KitSniper.java
@@ -12,22 +12,31 @@ import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Pose;
 import org.bukkit.entity.Projectile;
 import org.bukkit.entity.AbstractArrow.PickupStatus;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.projectiles.ProjectileSource;
 import org.bukkit.util.Vector;
 
 import me.toomuchzelda.teamarenapaper.Main;
 import me.toomuchzelda.teamarenapaper.teamarena.PlayerInfo;
+import me.toomuchzelda.teamarenapaper.teamarena.damage.DamageEvent;
+import me.toomuchzelda.teamarenapaper.teamarena.damage.DamageType;
 import me.toomuchzelda.teamarenapaper.teamarena.kits.abilities.Ability;
 import me.toomuchzelda.teamarenapaper.teamarena.preferences.Preferences;
 import me.toomuchzelda.teamarenapaper.utils.ItemUtils;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
+import net.minecraft.world.item.alchemy.Potion;
 
 /**
  * @author onett425
@@ -36,12 +45,13 @@ public class KitSniper extends Kit {
 
     public static int GRENADE_TIME = 40;
     public static boolean GRENADE_ARMED = false;
+    public static boolean SNIPER_CHAT_MESSAGE_SENT = false;
 
     public KitSniper() {
         super("Sniper", "mlg", Material.SPYGLASS);
 
         ItemStack[] armour = new ItemStack[4];
-        armour[3] = new ItemStack(Material.LEATHER_HELMET);
+        armour[3] = new ItemStack(Material.TURTLE_HELMET);
         armour[2] = new ItemStack(Material.LEATHER_CHESTPLATE);
         armour[1] = new ItemStack(Material.LEATHER_LEGGINGS);
         armour[0] = new ItemStack(Material.LEATHER_BOOTS);
@@ -52,7 +62,7 @@ public class KitSniper extends Kit {
         ItemMeta rifleMeta = rifle.getItemMeta();
         rifleMeta.displayName(ItemUtils.noItalics(Component.text("CheyTac Intervention")));
         rifle.setItemMeta(rifleMeta);
-        ItemStack grenade = new ItemStack(Material.DRIED_KELP_BLOCK);
+        ItemStack grenade = new ItemStack(Material.TURTLE_HELMET);
         ItemMeta grenadeMeta = grenade.getItemMeta();
         grenadeMeta.displayName(ItemUtils.noItalics(Component.text("Frag Grenade")));
         grenade.setItemMeta(grenadeMeta);
@@ -64,16 +74,14 @@ public class KitSniper extends Kit {
     public static class SniperAbility extends Ability{
         @Override
 		public void giveAbility(Player player) {
-			//Removed while testing
-            //player.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).addModifier(KitNinja.NINJA_SPEED_MODIFIER);
             player.setExp(0.999f);
             GRENADE_ARMED = false;
 		}
 		
 		@Override
 		public void removeAbility(Player player) {
-			//Removed while testing
-            //player.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).removeModifier(KitNinja.NINJA_SPEED_MODIFIER);
+            // remove below line after testing
+            player.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).removeModifier(KitNinja.NINJA_SPEED_MODIFIER);
             player.setExp(0);
             GRENADE_ARMED = false;
 		}
@@ -84,20 +92,11 @@ public class KitSniper extends Kit {
            Player player = event.getPlayer();
            Action action = event.getAction();
            PlayerInfo pinfo = Main.getPlayerInfo(player);
-            //Sniper Rifle is fired
-            if(player.getInventory().getItemInMainHand().getType() == Material.SPYGLASS && player.hasCooldown(Material.SPYGLASS)){
-                event.setCancelled(true);
-            }
-            else if(player.getInventory().getItemInMainHand().getType() == Material.SPYGLASS && (action.equals(Action.LEFT_CLICK_AIR) || action.equals(Action.LEFT_CLICK_BLOCK) && !player.hasCooldown(Material.SPYGLASS))){
-                Vector velocity = player.getLocation().getDirection().normalize().multiply(8D);
-                player.launchProjectile(Arrow.class, velocity).setPickupStatus(PickupStatus.DISALLOWED);
-                player.setCooldown(Material.SPYGLASS, (int) (2.5*20));
-            }
             
-            //Grenade Pull Pin
-            if(mat == Material.DRIED_KELP_BLOCK && !player.hasCooldown(Material.DRIED_KELP_BLOCK) && player.getExp() == 0.999f){
-                Component text = Component.text("Left Click to THROW    ").color(TextColor.color(242, 44, 44));
-                text.append(Component.text("Right Click to TOSS").color(TextColor.color(235, 152, 89)));  
+            
+           //Grenade Pull Pin
+            if(mat == Material.TURTLE_HELMET && !player.hasCooldown(Material.TURTLE_HELMET) && player.getExp() == 0.999f ){
+                Component text = Component.text("Left Click to THROW    Right Click to TOSS").color(TextColor.color(242, 44, 44));
                 if (pinfo.getPreference(Preferences.KIT_ACTION_BAR)) {
 					player.sendActionBar(text);
 				}
@@ -105,40 +104,21 @@ public class KitSniper extends Kit {
 					player.sendMessage(text);
 				}
                 player.setExp(0);
-                player.setCooldown(Material.DRIED_KELP, 2 * 20);
+                player.setCooldown(Material.TURTLE_HELMET, 2 * 20);
                 GRENADE_ARMED = true;
                 
             }
             //Grenade Throw
-            if(mat == Material.DRIED_KELP_BLOCK && player.hasCooldown(Material.DRIED_KELP_BLOCK)){
+            if(mat == Material.TURTLE_HELMET && player.hasCooldown(Material.TURTLE_HELMET)){
                 //Left Click => Hard Throw
                 if(action.equals(Action.LEFT_CLICK_AIR) || action.equals(Action.LEFT_CLICK_BLOCK)){
-                    player.getInventory().removeItemAnySlot(new ItemStack(Material.DRIED_KELP_BLOCK));
+                    player.getInventory().removeItemAnySlot(new ItemStack(Material.TURTLE_HELMET));
                 }
                 //Right Click => Soft Toss
                 if(action.equals(Action.RIGHT_CLICK_AIR) || action.equals(Action.RIGHT_CLICK_BLOCK)){
-                    player.getInventory().removeItemAnySlot(new ItemStack(Material.DRIED_KELP_BLOCK));
+                    player.getInventory().removeItemAnySlot(new ItemStack(Material.TURTLE_HELMET));
                 }
 
-            }
-        }
-
-        @Override
-        public void onLaunchProjectile(PlayerLaunchProjectileEvent event) {
-            Projectile projectile = event.getProjectile();
-            Player player = event.getPlayer();
-            ItemStack item = event.getItemStack();
-            //Sniper Arrow Properties
-            if(item.getType() == Material.SPYGLASS && projectile instanceof Arrow){
-                if(player.hasCooldown(Material.SPYGLASS)){
-                    event.setCancelled(true);
-                }
-                Arrow arrow = (Arrow) projectile;
-                arrow.setGravity(false);
-                arrow.setPierceLevel(5);
-                arrow.setKnockbackStrength(1);
-                arrow.setCritical(false);
-                arrow.setDamage(12);
             }
         }
 
@@ -148,18 +128,56 @@ public class KitSniper extends Kit {
             Projectile projectile = event.getEntity();
             Entity victim = event.getCollidedWith();
             if(victim instanceof Player player && projectile.getType() == EntityType.ARROW){
-                //Head region is the top 1/4 of the player's body
-                double headLocation = player.getBoundingBox().getCenterY() + 0.5D;
-                Location projectileHitLoc = projectile.getLocation();
-                if(Math.abs(projectileHitLoc.getY() - headLocation) <= 0.5){
+                double headLocation = player.getLocation().getY();
+                double projectileHitY = projectile.getLocation().getY();
+                //Every shot is considered a headshot if you are too close
+                if(projectileHitY - headLocation > 1.35d && projectile.getOrigin().distance(projectile.getLocation()) > 10){
                     player.damage(999);
                 }
+            }
+        }
+
+        public void playerDropItem(PlayerDropItemEvent event) {
+            Player player = event.getPlayer();
+            if(event.getItemDrop().getItemStack().getType() == Material.SPYGLASS && !event.getPlayer().hasCooldown(Material.SPYGLASS)){
+                Vector velocity = player.getLocation().getDirection().normalize().multiply(15d);
+                player.launchProjectile(Arrow.class, velocity).setPickupStatus(PickupStatus.DISALLOWED);
+                player.setCooldown(Material.SPYGLASS, (int) (2.5*20));
             }
         }
 
         @Override
         public void onPlayerTick(Player player) {
             float exp = player.getExp();
+            
+            //Speed based on held item
+            if(player.getInventory().getItemInMainHand().getType() == Material.IRON_SWORD){
+                player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 123123, 1));
+            }
+            else{
+                if(player.hasPotionEffect(PotionEffectType.SPEED)){
+                    player.removePotionEffect(PotionEffectType.SPEED);
+                }
+            }
+
+            //Sniper Action Bar
+            if(player.getInventory().getItemInMainHand().getType() == Material.SPYGLASS){
+                Component text = Component.text("Drop Item to Shoot").color(TextColor.color(89, 237, 76));
+                PlayerInfo pinfo = Main.getPlayerInfo(player);
+                if (pinfo.getPreference(Preferences.KIT_ACTION_BAR)) {
+					player.sendActionBar(text);
+				}
+				if(pinfo.getPreference(Preferences.KIT_CHAT_MESSAGES) && !SNIPER_CHAT_MESSAGE_SENT) {
+					player.sendMessage(text);
+                    SNIPER_CHAT_MESSAGE_SENT = true;
+				}
+            }
+            //Sniper Shoot
+            if(player.getInventory().getItemInMainHand().getType() == Material.SPYGLASS && player.getPose() == Pose.SNEAKING && !player.hasCooldown(Material.SPYGLASS)){
+                Vector velocity = player.getLocation().getDirection().normalize().multiply(10d);
+                player.launchProjectile(Arrow.class, velocity).setPickupStatus(PickupStatus.DISALLOWED);
+                player.setCooldown(Material.SPYGLASS, (int) (2.5*20));
+            }
             //Grenade Cooldown
             if(!GRENADE_ARMED){
                 //10 second cooldown = 200 ticks
